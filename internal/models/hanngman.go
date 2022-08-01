a591bb7c8c4ef95e710cae21352eb5833ca6aa30
hbourgeotjr <henrrybrgt@gmail.com>
2022-07-31 21:29:51 -0400

    Added template and models package

---
+++ internal/models/hanngman.go
@@ -0,0 +1,17 @@
+package models
+
+func GetSingleRecord(id int) error {
+	return nil
+}
+
+func GetLatestRecords() error {
+	return nil
+}
+
+func Insert() error {
+	return error
+}
+
+func UpdateQuestions() error {
+	return nil
+}
