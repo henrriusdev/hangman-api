ce5479ada87923d4765e844cd210ebda97e7e175
hbourgeotjr <henrrybrgt@gmail.com>
2022-08-02 17:21:48 -0400

    Added GetQuestion(id int) for our hangman API

--- internal/models/hanngman.go
+++ internal/models/hanngman.go
@@ -1,17 +1,55 @@
 package models
 
-func GetSingleRecord(id int) error {
-	return nil
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+	"log"
+)
+
+type Questions struct {
+	ID           int
+	Question     string
+	Answer       string
+	AnswerLenght int
 }
 
-func GetLatestRecords() error {
-	return nil
+func makeCN() (*sql.DB, error) {
+	connection := "user=postgres dbname=hangman password=Reyshell host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", connection)
+	if err != nil {
+		return nil, err
+	} else if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	log.Println("Connected")
+}
+
+func GetQuestion(id int) []Questions {
+	db, err := makeCN()
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+
+	query := "SELECT hint, answer, answer_lenght FROM quests WHERE id = $1"
+	row := db.QueryRow(query, id)
+	if row.Err() != nil {
+		log.Fatal(err)
+		return nil
+	}
+
+	var quest Questions
+	err = row.Scan(&quest.ID, &quest.Question, &quest.Answer, &quest.AnswerLenght)
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+	var hangman []Questions
+	hangman = append(hangman, quest)
+
+	return hangman
 }
 
 func Insert() error {
 	return error
-}
-
-func UpdateQuestions() error {
-	return nil
 }
